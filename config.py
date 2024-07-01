263792da99a23cb929d75c34f0dd0dd2794d7d13
Gady Margalit <gady@margalit.ws>
2024-07-01 22:24:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ ACCOUNT_ID="PlaceyourAccoutID"
 # Generating an API Key - https://help.smartsuite.com/en/articles/4855681-generating-an-api-key
 
 #DESK_FOLDER=Path.home() / 'smartsuite'
-DESK_FOLDER=Path.home() / 'smartsuite/backup'
+DEST_FOLDER=Path.home() / 'smartsuite/backup'
